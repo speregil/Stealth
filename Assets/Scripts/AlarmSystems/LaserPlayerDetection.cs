f1f8b8d24d775a9d8bd8f3ead4c088c081ec7f2a
Sebastian Gil Parga <s.gilparga@gmail.com>
2013-09-14 10:09:35 -0500

    Proyecto inicial

---
+++ Assets/Scripts/AlarmSystems/LaserPlayerDetection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserPlayerDetection : MonoBehaviour
+{
+    private GameObject player;                          // Reference to the player.
+    private LastPlayerSighting lastPlayerSighting;      // Reference to the global last sighting of the player.
+
+
+    void Awake ()
+    {
+        // Setting up references.
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        // If the beam is on...
+        if(renderer.enabled)
+            // ... and if the colliding gameobject is the player...
+            if(other.gameObject == player)
+                // ... set the last global sighting of the player to the colliding object's position.
+                lastPlayerSighting.position = other.transform.position;
+    }
+}
